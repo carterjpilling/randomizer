36ebbb0ad5833960912732d21c110c07deecb55f
Cj <carterjpilling@gmail.com>
2021-01-19 20:19:58 -0700

    Did some more postman testing

--- server/ListController.js
+++ server/ListController.js
@@ -3,9 +3,8 @@ module.exports = {
     const db = req.app.get('db')
     const { category, list } = req.body
 
-    const [array] = list
 
-    await db.new_list([category, array])
+    await db.new_list([category, list])
 
     res.sendStatus(200)
   },
